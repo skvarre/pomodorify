f16a9c908c0738d57c41cb9dac76c1712f84762f
skvarre <skvaertill@hotmail.com>
2023-11-14 20:24:55 -0600

    Render DOM based on logged in/or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SpotifyAuth from './SpotifyAuth';
 
 function App() {
+  const [token, setToken] = useState<string>('');
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
 return (
     <div className="">
       <header className="">
-        <SpotifyAuth /> 
+        {(token === '') ? <SpotifyAuth /> : <h1>Logged in</h1> }
       </header>
     </div>
   );
--- src/WebPlayback.tsx
+++ src/WebPlayback.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from "react";
 
-const WebPlayback = (props) => {
+const WebPlayback = ( ) => {
     
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://sdk.scdn.co/spotify-player.js";
+        script.async = true;
+    });
 }
