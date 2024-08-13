a56e2f868e080fb1fafc3f6a91e30df5a76b6153
Tim Olsén <skvaertill@hotmail.com>
2024-08-13 15:54:10 +0200

    Add tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+interface TooltipProps {
+  text: string;
+  children: React.ReactNode;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({ text, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <div
+        onMouseEnter={() => setIsVisible(true)}
+        onMouseLeave={() => setIsVisible(false)}
+      >
+        {children}
+      </div>
+      {isVisible && (
+        <div className="absolute z-10 px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm tooltip dark:bg-gray-700 bottom-full left-1/2 transform -translate-x-1/2 -translate-y-2">
+          {text}
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
